3d09ec5df983b7d343c52f9baca12ca1c3d618d0
Charlie <charlotte.badesco@epitech.eu>
2024-12-24 17:09:23 +0100

    adding an about me component, skills,and a changing header pic

--- nextjs-tailwind-resume-page-main/src/app/hero.tsx
+++ nextjs-tailwind-resume-page-main/src/app/hero.tsx
@@ -1,83 +1,134 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import {
   Avatar,
   Button,
   Card,
   CardBody,
   CardHeader,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
   Typography,
 } from "@material-tailwind/react";
 
 function Hero() {
+  const [open, setOpen]=useState(false);
+  const handleOpen = () => setOpen(!open);
   return (
-    <section id="hero" className="container mx-auto px-8 py-10 bg-gray-900 text-gray-100" style={{"maxWidth": "100%"}}>
+    <section
+      id="hero"
+      className="container mx-auto px-8 py-10 bg-gray-900 text-gray-100"
+      style={{ maxWidth: "100%" }}
+    >
       <Card shadow={false} className="border border-gray-700 rounded-2xl bg-gray-800">
         <CardHeader shadow={false} className="h-60 !rounded-lg">
           <img
-            src="image/dark-image.jpg"
-            alt="dark"
-            height={1024}
-            width={1024}
-            className="w-full h-full object-center"
+            src="./image/strip.jpg"
+            alt="bannière"
+            className="w-full h-full object-cover object-center"
           />
         </CardHeader>
         <CardBody>
           <div className="flex lg:gap-0 gap-6 flex-wrap justify-between items-center">
             <div className="flex items-center gap-3">
-              <Avatar src="image/avatar1.png" alt="avatar" variant="rounded" />
+              <Avatar src="./image/avatar1.png" alt="avatar" variant="rounded" />
               <div>
                 <Typography variant="h6" color="white">
                   Charlie Badesco
                 </Typography>
-                <Typography
-                  variant="small"
-                  className="font-normal text-gray-400"
-                >
+                <Typography variant="small" className="font-normal text-gray-400">
                   charlotte.badesco@gmail.com
                 </Typography>
-                <Typography
-                  variant="small"
-                  className="font-normal text-gray-500"
-                >
-                  Lyon 69007 _véhiculée
+                <Typography variant="small" className="font-normal text-gray-500">
+                  Lyon 69007
+                  <br/>
+                  Permis B, véhiculée
                 </Typography>
               </div>
             </div>
-            
+
             <div className="flex flex-wrap items-center gap-2">
-            <a href="https://github.com/Charliebadesco" target="_blank" rel="noopener noreferrer">
-              <Button
-                variant="outlined"
-                className="border-gray-600 text-gray-300 flex items-center gap-2"
+              <a
+                href="https://github.com/Charliebadesco"
+                target="_blank"
+                rel="noopener noreferrer"
               >
-                <i className="fa fa-github text-base" />
-                Github
-              </Button>
+                <Button
+                  variant="outlined"
+                  className="border-gray-600 text-gray-300 flex items-center gap-2"
+                >
+                  <i className="fa fa-github text-base" />
+                  Github
+                </Button>
               </a>
-              <a href="https://www.linkedin.com/in/charlie-badesco-2a25441ab/"target="_blank" rel="noopener noreferrer">
-              <Button
-                variant="outlined"
-                className="border-gray-600 text-gray-300 flex items-center gap-2"
+              <a
+                href="https://www.linkedin.com/in/charlie-badesco-2a25441ab/"
+                target="_blank"
+                rel="noopener noreferrer"
               >
-                <i className="fa-brands fa-linkedin" />
-                LinkedIn
-              </Button>
+                <Button
+                  variant="outlined"
+                  className="border-gray-600 text-gray-300 flex items-center gap-2"
+                >
+                  <i className="fa-brands fa-linkedin" />
+                  LinkedIn
+                </Button>
               </a>
             </div>
-            
           </div>
-          <Typography
-            variant="small"
-            className="font-normal text-gray-400 mt-6"
-          >
-            Passionnée par l'informatique et le développement d'outils innovants.
+
+          <Typography variant="small" className="font-normal text-gray-400 mt-6">
+            Passionnée par l'informatique, les sciences et le développement d'outils innovants.
             <br />
-            En reconversion professionnelle, avec une volonté de faire de ma passion un métier.
+            Toujours curieuse, rigoureuse et animée par une soif d'apprentissage. Je suis en reconversion professionnelle dans l'informatique, j'ai une passion depuis l'adolescence pour le développement de nouvelles fonctionnalités avec une volonté de faire de ma passion un métier.
+            <br />
           </Typography>
+          <div className="mt-4">
+            <Button
+              variant="gradient"
+              color="gray"
+              onClick={handleOpen}
+              className="w-full md:w-auto"
+            >
+              En savoir plus sur moi
+            </Button>
+          </div>
         </CardBody>
       </Card>
+
+      {/* aboutme me modale*/}
+
+      <Dialog open={open} handler={handleOpen}>
+        <DialogHeader>Pour en Savoir plus</DialogHeader>
+        <DialogBody divider>
+          <Typography variant="paragraph" className="text-gray-800">
+              Bonjour ! Je suis Charlie, une jeune femme passionnée par les jeux vidéos, l'art, les sciences et l'informatique. 
+              <br/>
+              Depuis mon adolescence, je m'amuse a créer des petits jeux et mods dans différents langages. 
+              <br/>
+              J'ai décidé cette année de me lancer dans une reconversion professionnelle afin de faire de mes loisirs un métier et pouvoir continuer a développer mes compétences pour créer des solutions impactantes pour plus de personnes.
+              <br/>
+              <br/>
+              Mes loisirs sont diversent et incluent la musique, les casses-têtes, puzzles et jeux de constructions mais également la découverte de concepts scientifiques complexes et leur simulation graphique.
+              <br/>
+              <br/>
+              J'apprécie également les sorties en pleine nature, les randonnées et le bivouac. Je possède une voiture et le permis ce qui me permet d'être completement mobile dans la région.
+          </Typography>
+        </DialogBody>
+        <DialogFooter>
+          <Button
+          variant="outlined"
+            color="gray"
+            onClick={handleOpen}
+            className="mr-2"
+          >
+            Fermer
+          </Button>
+        </DialogFooter>
+      </Dialog>
     </section>
   );
 }
--- nextjs-tailwind-resume-page-main/src/app/information-section.tsx
+++ nextjs-tailwind-resume-page-main/src/app/information-section.tsx
@@ -14,14 +14,14 @@ const EDUCATION = [
     title: "Piscine Web, Java, Projet Hub - Epitech (69)",
     date: "2024",
     children:
-      "Développement web : front-end avec React, HTML/CSS, back-end avec MySQL/MariaDB. Développement Java : jeu vidéo 2D avec JavaFX.",
+    "Projets : création de sites web dynamiques avec React.js et Node.js, gestion de bases de données MariaDB. Piscines intensives : Web (HTML/CSS, PHP, JavaScript), Java (développement de jeux vidéo 2D), réseau et administration systèmes. Développement web : front-end avec React, HTML/CSS, back-end avec MySQL/MariaDB. Développement Java : jeu vidéo 2D avec JavaFX.",
   },
   {
     icon: AcademicCapIcon,
     title: "Master Hygiène Sécurité Environnement - CNAM (49)",
     date: "2023",
     children:
-      "Spécialité : Environnement, gestion des déchets, gestion des sites pollués.",
+      "Spécialité : Environnement, gestion des déchets, gestion des sites pollués. Projets : exercices de gestion de risque, gestion de projet, études de cas en site industriels. Enseignements : risques industriels et environnementaux, prévention des risques, analyse des milieux, ecologie, analyses de données statistiques. ",
   },
   {
     icon: AcademicCapIcon,
@@ -43,21 +43,21 @@ const EXPERIENCE = [
     title: "Animatrice Scientifique",
     date: "2024",
     children:
-      "Animation d'activités scientifiques pour enfants de 6 à 12 ans dans un site de montagne.",
+      "Animation d'activités scientifiques pour enfants de 6 à 12 ans dans un site de montagne. Création d’ateliers d’initiation à la robotique et au code (Arduino, Python)",
   },
   {
     icon: BriefcaseIcon,
     title: "Apprentissage Hygiène Sécurité Environnement - EFS (44)",
     date: "2021 - 2023",
     children:
-      "Création d'un outil d'aide à la décision pour optimiser la gestion des déchets. Accompagnement en rédaction de document unique et mise en place de l'outil Track Déchets.",
+      "Développement d’un outil de gestion des déchets et d’aide à la décision pour la gestion de prestataires. Collaboration avec l’équipe HSE pour la rédaction de documents techniques et leur digitalisation. Intégration de la plateforme TrackDéchets à l'échelle régionale. Aide à la rédaction du document unique. Participation à la revue de direction. ",
   },
   {
     icon: BriefcaseIcon,
     title: "Stagiaire en journalisme scientifique - Communicasciences (67)",
     date: "2021",
     children:
-      "Rédaction d'articles de vulgarisation scientifique pour la préservation de la faune du Grand Est.",
+      "Recherche et documentation sur des projets technologiques liés à l’environnement. Rédaction d’articles techniques sur des outils numériques pour la préservation des espèces. ",
   },
   {
     icon: BriefcaseIcon,
@@ -68,6 +68,18 @@ const EXPERIENCE = [
 ];
 
 const SKILLS = [
+  {
+    icon: FaceSmileIcon,
+    title: "Langues pratiquées",
+    date: "Compétences Techniques",
+    children: "Français (Langue maternelle), Anglais (C1-TOEIC 910), Allemand (A2), Roumain (A2), LSF(A2)",
+  },
+  {
+    icon: FaceSmileIcon,
+    title: "Langages et technologies informatiques",
+    date: "Compétences Techniques",
+    children: "Maîtrise de frameworks front-end tels que React, conpréhension du fonctionnement des bases de données, Expérientations dans tout type de technologies informatiquesPython, Java, PHP, TypeScript, React.js, Node.js, Express.js, Tailwind CSS, Visual Studio Code, IntelliJ IDEA, Eclipse, Linux, Windows, création de machines virtuelles, Algorithmie. ",
+  },
   {
     icon: FaceSmileIcon,
     title: "Développement FullStack",
